fbe9145ec9a59f2fa738fed172cb015b2d6fb96e
ecsd-gituser <ecsd.github@eagleschools.net>
2024-10-15 09:28:31 -0600

    Update background.js

--- background.js
+++ background.js
@@ -1,43 +1,55 @@
 chrome.runtime.onInstalled.addListener(() => {
-  startSessionCounter();
+    startSessionCounter();
 });
 
 chrome.runtime.onStartup.addListener(() => {
-  startSessionCounter();
+    startSessionCounter();
 });
 
 function startSessionCounter() {
-  chrome.storage.local.set({sessionCounter: 0});
-  chrome.alarms.create('sessionTick', {periodInMinutes: 1});
+    chrome.storage.local.set({sessionCounter: 0});
+    chrome.alarms.create('sessionTick', {periodInMinutes: 1});
 }
 
 chrome.alarms.onAlarm.addListener((alarm) => {
-  if (alarm.name === 'sessionTick') {
-    chrome.storage.local.get('sessionCounter', (result) => {
-      let counter = result.sessionCounter || 0;
-      counter++;
-      chrome.storage.local.set({sessionCounter: counter});
-      
-      if (counter >= 50) {
-        resetApp();
-      }
-    });
-  }
+    if (alarm.name === 'sessionTick') {
+        chrome.storage.local.get('sessionCounter', (result) => {
+            let counter = result.sessionCounter || 0;
+            counter++;
+            chrome.storage.local.set({sessionCounter: counter});
+            
+            if (counter >= 50) {
+                resetApp();
+            }
+        });
+    } else if (alarm.name === 'checkSessionStatus') {
+        chrome.storage.sync.get(['kioskSessionEnded'], function(result) {
+            if (result.kioskSessionEnded) {
+                resetApp();
+            }
+        });
+    }
 });
 
 function resetApp() {
-  chrome.storage.local.remove(['sessionCounter', 'sessionActive']);
-  chrome.tabs.query({}, (tabs) => {
-    tabs.forEach((tab) => {
-      chrome.tabs.reload(tab.id);
+    chrome.storage.local.remove(['sessionCounter', 'sessionActive']);
+    chrome.storage.sync.remove('kioskSessionEnded');
+    chrome.tabs.query({}, (tabs) => {
+        tabs.forEach((tab) => {
+            chrome.tabs.remove(tab.id);
+        });
     });
-  });
+    chrome.tabs.create({url: 'index.html'});
 }
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === "login") {
-    chrome.storage.local.set({sessionActive: true, sessionCounter: 0}, () => {
-      chrome.tabs.create({url: 'app.html'});
-    });
-  }
+    if (request.action === "login") {
+        chrome.storage.local.set({sessionActive: true, sessionCounter: 0}, () => {
+            chrome.tabs.create({url: 'app.html'});
+        });
+    }
+    return true; // Indicates that the response is sent asynchronously
 });
+
+// Create an alarm to check session status every 30 seconds
+chrome.alarms.create('checkSessionStatus', { periodInMinutes: 0.5 });

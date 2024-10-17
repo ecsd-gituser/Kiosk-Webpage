fb529d2d892ff65ffab05d33bd5986d78ea2b665
ecsd-gituser <ecsd.github@eagleschools.net>
2024-10-17 09:43:33 -0600

    Update background.js

--- background.js
+++ background.js
@@ -2,37 +2,23 @@ console.log('Background script loaded');
 
 chrome.runtime.onInstalled.addListener(() => {
   console.log('Extension installed. Creating initial alarm.');
-  chrome.alarms.create('sessionExpiration', { delayInMinutes: 1 });
+  chrome.alarms.create('idleCheck', { periodInMinutes: 1 });
 });
 
 chrome.alarms.onAlarm.addListener((alarm) => {
-  if (alarm.name === 'sessionExpiration') {
-    console.log('Session expiration alarm triggered');
-    chrome.tabs.query({}, (tabs) => {
-      tabs.forEach((tab) => {
-        console.log('Redirecting tab to logout page:', tab.id);
-        chrome.tabs.update(tab.id, { url: 'logout.html' });
-      });
+  if (alarm.name === 'idleCheck') {
+    chrome.idle.queryState(60, (state) => {
+      if (state === 'idle') {
+        console.log('Device is idle. Initiating shutdown.');
+        chrome.power.requestShutdown();
+      }
     });
   }
 });
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === "startTimer") {
-    clearTimeout(logoutTimer);
-    logoutTimer = setTimeout(() => {
-      chrome.tabs.query({}, (tabs) => {
-        tabs.forEach(tab => {
-          chrome.tabs.update(tab.id, {url: 'logout.html'});
-        });
-      });
-    }, 60000); // 1 minute
-    sendResponse({status: "Timer started"});
-  } else if (message.action === "resetTimer") {
-    chrome.alarms.clear('sessionExpiration', (wasCleared) => {
-      chrome.alarms.create('sessionExpiration', { delayInMinutes: 1 });
-      sendResponse({status: "Timer reset"});
-    });
-    return true;
+  if (message.action === "resetIdleTimer") {
+    chrome.idle.setDetectionInterval(60);
+    sendResponse({status: "Idle timer reset"});
   }
 });

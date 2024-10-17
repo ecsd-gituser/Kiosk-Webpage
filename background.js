e4960760548060936ba5c8c29292e017488b205e
ecsd-gituser <ecsd.github@eagleschools.net>
2024-10-17 14:09:05 -0600

    Update background.js

--- background.js
+++ background.js
@@ -1,43 +1,45 @@
 let globalTimer;
+const APP_REFERENCE = 'app_ref=ecsd_kiosk';
 
 function startGlobalTimer() {
   clearTimeout(globalTimer);
   globalTimer = setTimeout(() => {
     chrome.tabs.query({}, (tabs) => {
       tabs.forEach((tab) => {
-        chrome.tabs.update(tab.id, { url: 'index.html' });
+        chrome.tabs.update(tab.id, { url: 'logout.html' });
       });
     });
   }, 60000); // 1 minute
 }
 
-chrome.runtime.onInstalled.addListener(() => {
-  console.log('Extension installed. Starting global timer.');
-  startGlobalTimer();
+function addAppReference(details) {
+  let url = new URL(details.url);
+  if (!url.searchParams.has('app_ref')) {
+    url.searchParams.append('app_ref', 'ecsd_kiosk');
+    return { redirectUrl: url.toString() };
+  }
+}
+
+chrome.webNavigation.onBeforeNavigate.addListener((details) => {
+  if (details.url.includes('mail.google.com') && !details.url.includes(APP_REFERENCE)) {
+    chrome.tabs.update(details.tabId, { url: details.url + (details.url.includes('?') ? '&' : '?') + APP_REFERENCE });
+  }
 });
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status === 'complete') {
     startGlobalTimer();
-  }
-});
-
-chrome.tabs.onActivated.addListener(() => {
-  startGlobalTimer();
-});
-
-// Add this to reset timer on user activity
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === 'complete') {
-    chrome.tabs.executeScript(tabId, {
-      code: `
-        ['click', 'keydown', 'mousemove', 'scroll'].forEach(event => {
-          document.addEventListener(event, function() {
-            chrome.runtime.sendMessage({action: "resetTimer"});
+    if (tab.url.includes(APP_REFERENCE)) {
+      chrome.tabs.executeScript(tabId, {
+        code: `
+          ['click', 'keydown', 'mousemove', 'scroll'].forEach(event => {
+            document.addEventListener(event, function() {
+              chrome.runtime.sendMessage({action: "resetTimer"});
+            });
           });
-        });
-      `
-    });
+        `
+      });
+    }
   }
 });
 
@@ -46,3 +48,9 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     startGlobalTimer();
   }
 });
+
+chrome.webRequest.onBeforeRequest.addListener(
+  addAppReference,
+  {urls: ["<all_urls>"]},
+  ["blocking"]
+);

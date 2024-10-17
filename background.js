46c71f8bc8c402bdee705b5ed4ec53a239d617cc
ecsd-gituser <ecsd.github@eagleschools.net>
2024-10-17 14:50:12 -0600

    Update background.js

--- background.js
+++ background.js
@@ -3,22 +3,16 @@ let timer;
 function startTimer() {
   clearTimeout(timer);
   timer = setTimeout(() => {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      if (tabs[0] && !tabs[0].url.includes('index.html')) {
-        chrome.tabs.update(tabs[0].id, {url: 'logout.html'});
-      }
+    chrome.tabs.query({}, (tabs) => {
+      tabs.forEach((tab) => {
+        chrome.tabs.update(tab.id, { url: 'logout.html' });
+      });
     });
   }, 60000); // 1 minute
 }
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status === 'complete' && !tab.url.includes('index.html')) {
-    startTimer();
-  }
-});
-
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === "resetTimer") {
     startTimer();
   }
 });
